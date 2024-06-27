a4546743b150c71a1a0b16d972b3312ad16e53a1
Lucio F. da Silva <sandmanlucio@gmail.com>
2024-06-27 02:25:49 -0300

    Error: EAFP, try, except

--- errors.py
+++ errors.py
@@ -3,22 +3,48 @@
 import os
 import sys
 
+# EAFP - Easy to Ask Forgiveness than permission
+# (É mais fácil perdir perdão do que permissão)
 
-#names = ["Bruno", "Maria", "Joao"]
-# LBYL - Look Before You Leap
 
-if os.path.exists("names.txt"):
-    print("O arquivo existe")
-    input("...") # race condition
-    names = open("names.txt").readlines()
-else:
-    print("[Error] Missing name in the list")
+try:
+    raise RuntimeError("Ocorreu um erro")
+except Exception as e:
+    print(str(e))
+
+
+try:
+    raise ValueError
+finally:
+    print("Execute isso sempre!")
+
+
+
+try:
+    names = open("names.txt").readlines() # FileNotFoundError
+    1/1 #ZeroDivisionError
+#    print(names.banana) #AttributeError
+    print(names.append) #AttributeError
+except FileNotFoundError as e: #Bare except
+    print(f"{str(e)} Missing name in the list")
     sys.exit(1)
-    
-if len(names) >= 3:
+    # TODO: usar retry
+except ZeroDivisionError:
+    print("[Error] You cant divide by zero")
+    sys.exit(1)
+except (ZeroDivisionError, AttributeError):
+    print("[Error] List doesn't have banana")
+    sys.exit(1)
+else:
+    print("Sucesso!")
+finally:
+    print("Execute isso sempre!")
+
+
+try:
     print(names[2])
-else:
+except:
     print("[Error] Missing name in the list")
     sys.exit(1)
     
-print(names[3])
+print(names[2])
